ddc0f43ad931fbc73ccfd56235d7f4fa412931b6
anangkur <anangk97@gmail.com>
2021-04-29 20:55:17 +0700

    fix(app): make button in MainActivity.kt fillMaxWidth()

--- app/src/main/java/com/anangkur/jcanimation/MainActivity.kt
+++ app/src/main/java/com/anangkur/jcanimation/MainActivity.kt
@@ -3,10 +3,7 @@ package com.anangkur.jcanimation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -32,12 +29,12 @@ class MainActivity : AppCompatActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Button(
-                            modifier = Modifier.padding(10.dp),
+                            modifier = Modifier.padding(10.dp).fillMaxWidth(),
                             onClick = { AnimatedVisibilityActivity.startActivity(this@MainActivity) },
                             content = { Text(text = getString(R.string.title_animated_visibility)) }
                         )
                         Button(
-                            modifier = Modifier.padding(10.dp),
+                            modifier = Modifier.padding(10.dp).fillMaxWidth(),
                             onClick = { AnimateContentSizeActivity.startActivity(this@MainActivity) },
                             content = { Text(text = getString(R.string.title_animated_content_size)) }
                         )
